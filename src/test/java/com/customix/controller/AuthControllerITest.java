d9c2c77b3ca9269e8821ff6d83342c419d2c8238
Jelvin Ferreras <jelviferreras01@gmail.com>
2026-04-30 09:03:17 -0400

    Add login integration test and enhance registration test in AuthController

--- src/test/java/com/customix/controller/AuthControllerITest.java
+++ src/test/java/com/customix/controller/AuthControllerITest.java
@@ -1,16 +1,24 @@
 package com.customix.controller;
 
-
-import com.customix.domain.enums.Role;
+import com.customix.domain.repo.UserRepo;
 import com.customix.dto.LoginDTO;
 import com.customix.dto.RegistrationDTO;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.isA;
 
 public class AuthControllerITest extends ControllerIT {
 
+    @Autowired
+    private UserRepo userRepo;
+
     private final RegistrationDTO.Request testRegRequest;
+    private final LoginDTO.Request testLoginRequest;
 
     public AuthControllerITest() {
         super();
@@ -20,10 +28,20 @@ public class AuthControllerITest extends ControllerIT {
                 null
         );
 
+        testLoginRequest = new LoginDTO.Request(
+                "testUser",
+                "testPassword01!"
+        );
+
+    }
+
+    @AfterEach
+    void tearDown() {
+        userRepo.deleteAll();
     }
 
     @Test
-    void shouldRegister() {
+    void shouldSuccessfullyRegister() {
 
         given()
             .contentType("application/json")
@@ -31,8 +49,28 @@ public class AuthControllerITest extends ControllerIT {
         .when()
             .post("/api/auth/registration")
         .then()
-            .statusCode(201);
+            .statusCode(HttpStatus.CREATED.value())
+            .body("role", is("USER"));
 
+    }
+
+    @Test
+    void shouldSuccessfullyLogin() {
+
+        given()
+            .contentType("application/json")
+            .body(testRegRequest)
+        .when()
+            .post("/api/auth/registration");
+
+        given()
+            .contentType("application/json")
+            .body(testLoginRequest)
+        .when()
+            .post("/api/auth/login")
+        .then()
+            .statusCode(HttpStatus.OK.value())
+            .body("token", isA(String.class));
 
     }
 
